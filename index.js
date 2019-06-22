acdd5792edf423a32a9642f5d73078b0253e4571
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-22 13:29:40 -0500

    got knex added to index.js; got my first endpoint, the POST, working. Taking a break for a bit.

--- index.js
+++ index.js
@@ -6,7 +6,50 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+const knex = require('knex');
+
+const config = {
+  client: 'sqlite3',
+  connection: {
+      filename: './data/lambda.db3'
+  },
+  useNullAsDefault: true
+};
+
+const db = knex(config);
+
+// knex functions here
+
+function find() {
+  return db('zoos');
+}
+
+function findById(id) {
+  return db('zoos').where({ id })
+}
+
+function add(name) {
+  return db('zoos').insert(name);
+}
+
+function update(id, changes) {
+  return db('zoos').where({ id }).update(changes);
+}
+
+function remove(id) {
+  return db('zoos').where({ id }).delete();
+}
+
+//endpoints to follow
+
+server.post('/api/zoos', (req, res) => {
+const zooAdd = req.body;
+  add(zooAdd)
+  .then(res.status(201).json({ zooAdd }))
+  .catch(res.status(500).json({ error: "Unable to add the entry."}))
+})
+
+
 
 const port = 3300;
 server.listen(port, function() {
