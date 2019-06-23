3e4f3bd45538e6c2dcddd1e475da8248e91801a8
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-23 18:43:03 -0500

    finished MVP, though all on one page; I can see why developers prefer to break this out into separate files

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const knex = require('knex');
 const config = {
   client: 'sqlite3',
   connection: {
-      filename: './data/lambda.db3'
+    filename: './data/lambda.db3'
   },
   useNullAsDefault: true
 };
@@ -43,15 +43,55 @@ function remove(id) {
 //endpoints to follow
 
 server.post('/api/zoos', (req, res) => {
-const zooAdd = req.body;
+  const zooAdd = req.body;
   add(zooAdd)
-  .then(res.status(201).json({ zooAdd }))
-  .catch(res.status(500).json({ error: "Unable to add the entry."}))
+    .then(res.status(201).json({ zooAdd }))
+    .catch(res.status(500).json({ error: "Unable to add the entry." }))
+})
+
+server.get('/api/zoos', async (req, res) => {
+  const zooList = await find();
+  if (zooList) {
+    (res.status(200).json({ zooList }))
+  }
+  else {
+    (res.status(500).json({ error: "Error retrieving zoos." }))
+  }
+})
+
+server.get('/api/zoos/:id', async (req, res) => {
+  const zoo = await findById(req.params.id);
+  if (zoo) {
+    (res.status(200).json({ zoo }))
+  }
+  else {
+    (res.status(500).json({ error: "Error retrieving zoo." }))
+  }
+})
+
+server.put('/api/zoos/:id', async (req, res) => {
+  try{
+      const zooEdit = await update(req.params.id, req.body);
+      res.status(201).json(zooEdit);
+  }
+  catch{
+      res.status(500).json({error: "Error editing zoo."})
+  }
+})
+
+server.delete('/api/zoos/:id', async (req, res) => {
+  try{
+      const zooDelete = await remove(req.params.id);
+      res.status(201).json({message: "Deleted!"});
+  }
+  catch{
+      res.status(500).json({error: "Error deleting action."})
+  }
 })
 
 
 
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
